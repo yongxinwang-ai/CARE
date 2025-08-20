b20c48e00a94463351c062567f252fed2ec4938b
Yongxin Wang <yongxin.wang@mbzuai.ac.ae>
2025-08-20 08:56:46 +0000

    cscg upd

--- run_test_rpp.sh
+++ run_test_rpp.sh
@@ -5,10 +5,10 @@
 #SBATCH --gres=gpu:8
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=128
-#SBATCH --output=qwen2_5_vl_3b_geo3k_grpo_cgsg.log
+#SBATCH --output=qwen2_5_vl_3b_geo3k_reinforce.log
 
 
 conda activate easy_r1
 
 # bash examples/qwen2_5_vl_7b_geo3k_cgsg_contrastive_grpo.sh
-bash examples/qwen2_5_vl_3b_geo3k_dapo.sh
+bash examples/qwen2_5_vl_3b_geo3k_reinforce.sh
